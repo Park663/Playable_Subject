d5fa3b6372506931326ab982e6ff774664c1a9b7
Park663 <parkdj663@gmail.com>
2025-07-08 23:24:38 +0900

    [feat] 아이템 상승 기능 구현
    아이템 상승 후 최고고도 도달 시 오브젝트 풀로 반환

--- Assets/@Scripts/GameManager.cs
+++ Assets/@Scripts/GameManager.cs
@@ -4,14 +4,56 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public bool isPlaying = false;
+    public static GameManager Instance { get; private set; }
 
+
+    public bool isPlaying = false;
+    public List<Items> items;
+    public List<Transform> itemTransform;
+    public float itemSpeed = 10f;
+    public float spawnInterval = 1f;
+
+    public Transform endPos;
+
+    public float spawnCount = 0;
+    public List<Transform> spawnPos = new List<Transform>();
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (!isPlaying) return;
+
+
+        SpawnItem();
+    }
+
+    private void SpawnItem()
+    {
+        if (spawnCount >= spawnInterval)
         {
-            ItemObjectPool.Instance.itemPool.Get();
+            spawnCount = 0;
+            
+            foreach (var v in spawnPos)
+            {
+                ItemObjectPool.Instance.itemPool.Get().transform.position = v.position;
+
+            }
+
+        }
+        else
+        {
+            spawnCount += Time.deltaTime;
         }
     }
+
+
 }
--- Assets/@Scripts/Items.cs
+++ Assets/@Scripts/Items.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 [Serializable]
 public struct MeshMap
 {
+    public ObejectType type;
     public Mesh mesh;
     public Material material;
 }
@@ -22,9 +23,12 @@ public enum ObejectType
 public class Items : MonoBehaviour
 {   
     [SerializeField] private List<MeshMap> meshes;
+    [SerializeField] private ObejectType objType;
 
     private MeshFilter meshFilter;
     private Renderer rendererMaterial;
+
+    private float moveSpeed = 1f;
 
     private void Awake()
     {
@@ -39,8 +43,16 @@ public class Items : MonoBehaviour
     private void RandomMesh()
     {
         int n = UnityEngine.Random.Range(0, (int)ObejectType.Count);
+        objType = (ObejectType)n;   
         meshFilter.mesh = meshes[n].mesh;
         rendererMaterial.material = meshes[n].material;
+    }
+    private void Update()
+    {
+        if (!GameManager.Instance.isPlaying) return;
+
+        if(transform.position.y >= GameManager.Instance.endPos.position.y) ItemObjectPool.Instance.itemPool.Release(gameObject);
+        transform.Translate(Vector2.up * GameManager.Instance.itemSpeed * Time.deltaTime);
     }
 }
 
